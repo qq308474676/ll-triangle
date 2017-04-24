4d2a7a1638d5e9fc56f11fa1259ea23b6b859805
qq308474676 <308474676@qq.com>
2017-04-24 13:30:19 +0800

    modified:   triangle/triangle.js

--- triangle/triangle.js
+++ triangle/triangle.js
@@ -1,22 +1,25 @@
 var triangle = {
     isEquilateral : function(a,b,c) {
-		if(a+b<c) return false ; 
+			if(a+b<c) return false ; 
 			if(a+c<b) return false ;
 			if(b+c<a) return false ;
+			
     	    if(a!=b) return false ; 
     	    if(b!=c) return false ;
     	    if(a<=0) return false ;
     		return true ;
     },
     isIsosceles : function(a,b,c) {
-		if(a+b<c) return false ; 
+			if(a+b<c) return false ; 
 			if(a+c<b) return false ;
 			if(b+c<a) return false ;
+			
 			if(a!=b){
 				if(b!=c) {
 					if(a!=c) return false ;
 				}
 			}
+			
 			if(a<=0){
 				if(b<=0){
 					if(c<=0) return false ;
@@ -28,6 +31,7 @@ var triangle = {
 			if(a+b<c) return false ; 
 			if(a+c<b) return false ;
 			if(b+c<a) return false ;
+			
 			if(a<=0){
 				if(b<=0){
 					if(c<=0) return false ;
